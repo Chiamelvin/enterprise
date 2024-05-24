79006ad1da671ccd0d3ea9f23c16a4f140d566f9
Chiamelvin <154296657+Chiamelvin@users.noreply.github.com>
2024-05-23 20:42:21 -0500

    welcome

---
+++ src/main/java/com/organization/App.java
@@ -0,0 +1,13 @@
+package com.organization;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
